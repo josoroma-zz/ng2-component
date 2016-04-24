b392a0625720a1eaec49581ecfcedc3f48a6a912
“josoroma” <pablo@josoroma.com>
2016-04-23 19:35:02 -0600

    Josoroma App Skeleton

---
+++ app/josoroma.app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'josoroma-app-component',
+    template: `
+      <header>
+        <h1>Header</h1>
+      </header>
+      <content>Content</content>
+      <footer>Footer</footer>
+    `
+})
+export class JosoromaAppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {JosoromaAppComponent} from './josoroma.app.component';
+
+bootstrap(JosoromaAppComponent);
