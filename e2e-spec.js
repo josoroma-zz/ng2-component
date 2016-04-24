9bb8ee9b4c6af3e80991bcf8033ddcbb78b690e9
“josoroma” <pablo@josoroma.com>
2016-04-23 19:31:08 -0600

    e2e-spec, karma-test-shim, karma, protractor and wallaby

---
+++ e2e-spec.js
@@ -0,0 +1,15 @@
+
+describe('Walkthrough E2E Tests', function () {
+
+	var expectedMsg = 'Angular 2 App';
+
+
+  beforeEach(function () {
+    browser.get('');
+  });
+
+  it('should display: ' + expectedMsg, function () {
+    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  });
+
+});
